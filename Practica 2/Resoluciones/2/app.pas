3ac80d25e13767761e42829237c883d7c335a91f
Manuel Tomas Teves <manuel.tomas.teves@gmail.com>
2025-08-18 11:26:51 -0300

    Punto 2 resuelto

--- Practica 2/Resoluciones/2/app.pas
+++ Practica 2/Resoluciones/2/app.pas
@@ -1,5 +1,13 @@
-program app
+program app;
+
+Function potencia1(x, n: integer): real;
+begin
+    if (n = 1) then
+        potencia1 := x
+    else
+        potencia1 := x * potencia1(x, n-1);
+end;
 
 begin
-    Writeln('Entorno preparado.');
-end;
+    writeln('5^3: ', potencia1(5, 3):2:2);
+end.
